8a9427f09220791dfdaa7716791306a401791f5a
yousofo <jon772002@gmail.com>
2025-06-02 19:28:46 +0300

    font size update

--- src/components/slides/HomeSlide/HomeSlide.js
+++ src/components/slides/HomeSlide/HomeSlide.js
@@ -1,8 +1,8 @@
 export default function HomeSlide() {
   return (
     <div className="name overflow-hidden sm:me-2 text-foreground transition-colors tracking-wide text-center">
-      <h2 className="font-bold text-5xl whitespace-nowrap">
-        Hello! <span className="text-4xl">I&apos;m Yousef</span>
+      <h2 className="font-bold text-4xl lg:text-5xl  ">
+        Hello! <span className="text-3xl lg:text-4xl">I&apos;m Yousef</span>
       </h2>
       <p className="offers text   mt-1 text  text-yellow-600 font-bold dark:text-yellow-300/80">
         {/* I&apos;m a <span>web developer</span> and I love building{" "}
