e648f500e3b26505ebb9e7fb5a4a1877cb12e494
yousofo <jon772002@gmail.com>
2025-06-02 19:32:06 +0300

    text size and home padding change

--- src/components/slides/HomeSlide/HomeSlide.js
+++ src/components/slides/HomeSlide/HomeSlide.js
@@ -2,7 +2,7 @@ export default function HomeSlide() {
   return (
     <div className="name overflow-hidden sm:me-2 text-foreground transition-colors tracking-wide text-center">
       <h2 className="font-bold text-4xl lg:text-5xl  ">
-        Hello! <span className="text-3xl lg:text-4xl">I&apos;m Yousef</span>
+        Hello! <span className="text-3xl lg:text-4xl">I&apos;m Youssef</span>
       </h2>
       <p className="offers text   mt-1 text  text-yellow-600 font-bold dark:text-yellow-300/80">
         {/* I&apos;m a <span>web developer</span> and I love building{" "}
